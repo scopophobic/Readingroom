395b5b2234f2ce47c134bcd07a0ae8bcb5c3fab6
scopophobic <adithyanmadhu1234@gmail.com>
2025-07-21 19:21:59 +0530

    updated requirements

--- Backend/readingroom/settings.py
+++ Backend/readingroom/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--244d^fvz=rtiorloo8f%r2=j6x07+=i8b8hbs&&fy7)@-@6zf"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
