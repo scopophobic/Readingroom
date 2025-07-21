42fbbb62962ee8a9959fabc200b13535d93ee4bb
scopophobic <adithyanmadhu1234@gmail.com>
2025-07-21 12:06:31 +0530

    added

--- Semantic-book-recommender/gradio-dashboard.py
+++ Semantic-book-recommender/gradio-dashboard.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from dotenv import load_dotenv
 import os
-from langchain.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 from langchain.vectorstores import Chroma
